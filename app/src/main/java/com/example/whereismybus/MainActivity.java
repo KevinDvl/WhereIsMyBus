0aaa8876d78b4d17c766d3ffda87af1eb930f067
Kevin <kevin@192.168.1.66>
2022-01-25 20:03:54 +0100

    JSON GET request

--- app/src/main/java/com/example/whereismybus/MainActivity.java
+++ app/src/main/java/com/example/whereismybus/MainActivity.java
@@ -1,14 +1,81 @@
 package com.example.whereismybus;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    //private Spinner selecteurLigne;
+    private TextView affichageResultat;
+    private Button sendButton;
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        affichageResultat = findViewById(R.id.affichageResultat);
+        sendButton = findViewById(R.id.sendButton);
+        //selecteurLigne = findViewById(R.id.selecteurLigne);
+
+        mQueue = Volley.newRequestQueue(this);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jsonParse();
+            }
+        });
     }
+
+    private void jsonParse() {
+        String url = "https://data.explore.star.fr/api/records/1.0/search/?dataset=tco-bus-topologie-lignes-td&q=&rows=100&sort=id";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("records");
+                            System.out.println(jsonArray.length());
+                            for(int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject ligneBus = jsonArray.getJSONObject(i);
+                                JSONObject fields = ligneBus.getJSONObject("fields");
+                                String idLigne = fields.getString("id");
+                                String nomLigne = fields.getString("nomcourt");
+
+                                affichageResultat.append("Id ligne : "+idLigne+"; Nom ligne : "+nomLigne+"\n");
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mQueue.add(request);
+    }
+
 }
